78f8d9cc6c07bf58df55fc2c507903409fa52fb0
varak <varaksinaalla@mail.ru>
2026-01-11 15:35:39 +0300

    Задание 2.33. Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -9,9 +9,9 @@ function checkStringLength(line = '', maxLength = 0) {
   return line.length <= maxLength;
 }
 
-// console.log(checkStringLength('проверяемая строка', 20));
-// console.log(checkStringLength('проверяемая строка', 18));
-// console.log(checkStringLength('проверяемая строка', 10));
+console.log(checkStringLength('проверяемая строка', 20));
+console.log(checkStringLength('проверяемая строка', 18));
+console.log(checkStringLength('проверяемая строка', 10));
 
 
 /**
@@ -27,15 +27,11 @@ function isPalindrom(line) {
   return lineBuffer == lineReversed;
 }
 
-// console.log(isPalindrom('топот'));
-// console.log(isPalindrom('ДовОд'));
-// console.log(isPalindrom('Кекс'));
-// console.log(isPalindrom('Лёша на полке клопа нашёл '));
+console.log(isPalindrom('топот'));
+console.log(isPalindrom('ДовОд'));
+console.log(isPalindrom('Кекс'));
+console.log(isPalindrom('Лёша на полке клопа нашёл '));
 
-// console.assert(isPalindrom ,'топот');
-// console.assert(isPalindrom ,'ДовОд');
-// console.assert(isPalindrom ,'Кекс');
-// console.assert(isPalindrom ,'Лёша на полке клопа нашёл ');
 
 /**
  * Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
@@ -65,12 +61,12 @@ function numberExtract(line) {
   return Math.abs(parseInt(result));
 }
 
-console.log('2023 год = ' + numberExtract('2023 год'));            // 2023
-console.log('ECMAScript 2022 = ' + numberExtract('ECMAScript 2022'));     // 2022
-console.log('1 кефир, 0.5 батона = ' + numberExtract('1 кефир, 0.5 батона')); // 105
-console.log('агент 007 = ' + numberExtract('агент 007'));           // 7
-console.log('а я томат = ' + numberExtract('а я томат'));           // NaN
+console.log('2023 год = ' + numberExtract('2023 год'));            //2023
+console.log('ECMAScript 2022 = ' + numberExtract('ECMAScript 2022'));     //2022
+console.log('1 кефир, 0.5 батона = ' + numberExtract('1 кефир, 0.5 батона')); //105
+console.log('агент 007 = ' + numberExtract('агент 007'));           //7
+console.log('а я томат = ' + numberExtract('а я томат'));           //NaN
 
-console.log('2023 = ' + numberExtract(2023));           // 2023
-console.log('-1 = ' + numberExtract(-1));           // 1
-console.log('1.5 = ' + numberExtract(1.5));           // 2023
+console.log('2023 = ' + numberExtract(2023));           //2023
+console.log('-1 = ' + numberExtract(-1));           //1
+console.log('1.5 = ' + numberExtract(1.5));           //2023
